46a09441bfc5a292474b11fba2e95525d707e7f9
Erick Bernardo <erickvictor@gmail.com>
2023-02-28 14:04:08 -0400

    Simple example of Calculator Rspec

---
+++ ex_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -0,0 +1,15 @@
+# in spec/calculator_spec.rb
+require 'calculator'
+RSpec.describe Calculator do
+  it 'use sum method for 2 numbers' do
+    calc = Calculator.new
+    result = calc.sum(5, 7)
+    expect(result).to eq(12)
+  end
+
+  specify 'use sum method for 2 numbers with negative numbers' do
+    calc = Calculator.new
+    result = calc.sum(-5, 7)
+    expect(result).to eq(2)
+  end
+end
